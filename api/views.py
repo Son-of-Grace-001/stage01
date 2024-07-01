6906478914f8a5191571762a931984aa02d63d43
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-07-01 20:58:34 +0100

    atlast

--- api/views.py
+++ api/views.py
@@ -8,13 +8,18 @@ def hello(request):
 
     # Get client IP
     client_ip = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+    
+    # Print the client IP for debugging
+    print(f"Client IP: {client_ip}")
+
+    ipgeolocation_api_key = os.getenv('IPGEOLOCATION_API_KEY')  # Ensure this environment variable is set
 
     try:
         if provided_city:
             city = provided_city
         else:
             # Get location info based on IP
-            location_response = requests.get(f'http://ipinfo.io/{client_ip}/json')
+            location_response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey=aa2cdeed7d77439f8d3be23a1d0bbfbf&ip={client_ip}')
             location_data = location_response.json()
 
             # Debugging information
@@ -23,7 +28,7 @@ def hello(request):
             city = location_data.get('city', 'Unknown City')
 
         if city == 'Unknown City' or 'bogon' in location_data:
-            city = 'Unknown City'  # Default city as fallback
+            city = 'Unknown city'  # Default city as fallback
             print(f"Using default city: {city}")
 
         # Get weather info
@@ -33,13 +38,13 @@ def hello(request):
         weather_data = weather_response.json()
 
         # Debugging information
-        print(f"Weather API Response: {weather_data}")
+        print(f"Weather API Response for city {city}: {weather_data}")
 
         if 'main' not in weather_data:
             response = {
                 "client_ip": client_ip,
                 "location": "Unknown City",
-                "greeting": f"Hello, {visitor_name}!, the temperature is N/A degrees Celsius in Unknown City "
+                "greeting": f"Hello, {visitor_name}!, the temperature is N/A degrees Celsius in Unknown City"
             }
         else:
             temperature = weather_data['main']['temp']
