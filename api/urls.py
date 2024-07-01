4b1a3147329022c8a164fc98471e0b61beaa0d51
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-07-01 17:37:01 +0100

    url

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import hello
 
 urlpatterns = [
-    path('hello/visitor_names =/<str:visitor_name>/', hello, name='hello'),
+    path("hello/visitor_name/<str:visitor_name>", hello, name='hello'),
 ]
